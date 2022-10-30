a35630099ca1815f877f99c829e2b1470eeeac0c
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-10-30 18:08:52 +0100

    Update JS file , the previous commit did not commit correctly

--- java.js
+++ java.js
@@ -1,18 +1,23 @@
 const btn = document.querySelector('.coin');
 const answer = document.querySelector('#result');
+const counterCoin = document.querySelector('#counter-coin');
+const counterTail = document.querySelector('#counter-tail');
 
 btn.addEventListener('click', coinFlip);
 
 
-function coinFlip (){
-    random = Math.floor(Math.random() * 2 ) + 1;
+function coinFlip() {
+    random = Math.floor(Math.random() * 2) + 1;
 
-    if (random === 1){
-        answer.textContent = 'Coin'
-    }else {
-        answer.textContent = 'Tails'
+    if (random === 1) {
+        answer.textContent = 'Coin', answer.style.color = 'blue';
+
+        counterCoin.textContent = +counterCoin.textContent + 1;
+    } else {
+        answer.textContent = 'Tails', answer.style.color = 'red';
+
+        counterTail.textContent = +counterTail.textContent + 1;
     }
-    
 }
 
 
