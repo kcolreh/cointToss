dc6d708075c38a6183d45aa3ce0742457d331d80
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-10-30 17:14:47 +0100

    Add a result text for user to see the output of the function

--- java.js
+++ java.js
@@ -1,19 +1,18 @@
-//Takes a random number from 1-2
-//Converts 1 to head
-//Converts 2 to tails
-//Displays result to the user
-
-
 const btn = document.querySelector('.coin');
+const answer = document.querySelector('#result');
+
 btn.addEventListener('click', coinFlip);
 
-function coinFlip (result){
+
+function coinFlip (){
     random = Math.floor(Math.random() * 2 ) + 1;
 
     if (random === 1){
-        result = 'Heads', console.log(result);
+        answer.textContent = 'Coin'
     }else {
-        result = 'Tails', console.log(result);
+        answer.textContent = 'Tails'
     }
     
 }
+
+
